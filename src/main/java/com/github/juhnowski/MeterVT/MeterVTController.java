c04b3c61537546fdec4fcb3e1ebe416dfd361697
Juhnowski <juhnowski@gmail.com>
2023-10-28 19:11:55 +0300

    add parameters

--- src/main/java/com/github/juhnowski/MeterVT/MeterVTController.java
+++ src/main/java/com/github/juhnowski/MeterVT/MeterVTController.java
@@ -1,22 +1,10 @@
 package com.github.juhnowski.MeterVT;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.MultipartBodyBuilder;
-import org.springframework.util.MultiValueMap;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.io.File;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-
-import java.net.URL;
-import java.util.List;
 import java.util.stream.IntStream;
 
 
@@ -25,9 +13,19 @@ public class MeterVTController {
 	@Autowired
     private WebClient.Builder webClientBuilder; 
 
+    @Value("${idx.from}")
+    private Integer idxFrom;
+
+    @Value("${idx.to}")
+    private Integer idxTo;
+
+    @Value("${app.url}")
+    private String testUri;
+
+
     @GetMapping(value = "/start")
     public void upload() {
-        IntStream.range(1, 1001).parallel().forEach(i -> {
+        IntStream.range(idxFrom, idxTo).parallel().forEach(i -> {
             try {
                 Thread.ofVirtual().name(""+i).start(new WebClientRunnable(webClientBuilder, ""+i)).join();;    
             } catch (Exception e) {
--- src/main/java/com/github/juhnowski/MeterVT/WebClientRunnable.java
+++ src/main/java/com/github/juhnowski/MeterVT/WebClientRunnable.java
@@ -1,27 +1,21 @@
 package com.github.juhnowski.MeterVT;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-
 import java.io.File;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-
-import java.net.URL;
-import java.util.List;
-import java.util.stream.IntStream;
 
 public class WebClientRunnable implements Runnable {
     private WebClient.Builder webClientBuilder;
     private String filename;
+
+    @Value("${app.url}")
+    private String testUri;
 
     public WebClientRunnable(WebClient.Builder webClientBuilder, String idx) {
         this.webClientBuilder = webClientBuilder;
@@ -32,7 +26,7 @@ public class WebClientRunnable implements Runnable {
         try {
         final WebClient webClient = webClientBuilder.build();
         webClient.patch()
-                .uri("http://127.0.0.1:8082/api/engagements")
+                .uri("testUri")
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .body(BodyInserters.fromMultipartData(fromFile(new File(filename))))
                 .retrieve()
